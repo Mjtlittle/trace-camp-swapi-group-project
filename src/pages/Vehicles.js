9bb5efa0d9ee7d0d3ea60b0251be8bd1cf00159d
Shreya <sthamm2001@gmail.com>
2020-06-15 16:45:17 -0400

    Vehicles Changes

--- src/pages/Vehicles.js
+++ src/pages/Vehicles.js
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, {Component} from 'react'
+import './Vehicles.css';
+import axios from 'axios';
 
-const Vehicles = () => {
-    return (
-        <div>
-        </div>
-    )
+
+class Vehicles extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {index:1,data:[]}
+        this.changeVeh = this.changeVeh.bind(this)
+    }
+
+    async componentDidMount() {
+        const url = `https://swapi.dev/api/vehicles/${this.state.index}/`;
+        let response = await axios.get(url);
+        console.log(response.data)
+        this.setState({index:this.state.index+1})
+        this.setState({data: response.data})
+    }
+
+    async changeVeh(){
+        this.setState({index:this.state.index+1})
+        const url = `https://swapi.dev/api/vehicles/${this.state.index}/`;
+        let response = await axios.get(url);
+        this.setState({data: response.data})
+    }
+
+    render(){
+        return (
+            <div className="Vehicles">
+                <h1 className='Vehicles-title'>LEARN ABOUT VEHICLES!</h1>
+                <div className = "Veh-container">
+                    <h3 className = "Vehicle-name">Vehicle Name: {this.state.data.name}</h3>
+                    <p>Vehicle Model: {this.state.data.model}</p>
+                    <p>Vehicle Manufacturer: {this.state.data.manufacturer}</p>
+                    <p>Vehicle Cost in Credits: {this.state.data.cost_in_credits}</p>
+                    <p>Vehicle Length: {this.state.data.length}</p>
+                    <p>Vehicle Max Atmosphering Speed: {this.state.data.max_atmosphering_speed}</p>
+                    <p>Vehicle Crew: {this.state.data.crew}</p>
+                    <p>Vehicle Passengers: {this.state.data.passengers}</p>
+                    <p>Vehicle Cargo Capacity: {this.state.data.cargo_capacity}</p>
+                </div>
+                <button className = "Vehicles-button" onClick={this.changeVeh}>Show me a vehicle!</button>
+            </div>
+        );
+    }
 }
 
-export default Vehicles
+export default Vehicles;
